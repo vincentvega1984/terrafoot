04c2251cc82c63698fe933e8ff8cad7944fc7ed8
Nikita Korotkiy <nikita.k@orot-technologies.co.il>
2020-02-28 01:20:05 +0200

    Hide snow, smaller font in menu

--- templates/terrafootball/index.php
+++ templates/terrafootball/index.php
@@ -67,17 +67,17 @@ defined('_JEXEC') or die;
 <?php if ($this->countModules( 'slider' )) : ?>
     <div class="slider">
         <jdoc:include type="modules" name="slider" style="xhtml" />
-        <div class="winter-is-coming">
         
-        <div class="snow snow--near"></div>
-        <div class="snow snow--near snow--alt"></div>
-        
-        <div class="snow snow--mid"></div>
-        <div class="snow snow--mid snow--alt"></div>
-        
-        <div class="snow snow--far"></div>
-        <div class="snow snow--far snow--alt"></div>
-        </div>
+        <!--For winter slider snow only-->
+        <!-- <div class="winter-is-coming">
+            <div class="snow snow--near"></div>
+            <div class="snow snow--near snow--alt"></div>
+            <div class="snow snow--mid"></div>
+            <div class="snow snow--mid snow--alt"></div>
+            <div class="snow snow--far"></div>
+            <div class="snow snow--far snow--alt"></div>
+        </div> -->
+
     </div>
 <?php endif; ?>
 <!--end of slider-->
